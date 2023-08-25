fa6be10c2b6a1cf3bb30af6625f5c78a182dcbff
laiba ahsan <laiba.ahsan27@gmail.com>
2023-08-25 20:25:12 +0530

    start

---
+++ js.js
@@ -0,0 +1,7 @@
+const but = document.querySelector('.button');
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".nav")
+const toggleNavBar=()=>{
+    nav_header.classList.toggle('activ');
+    but.classList.toggle('activu');
+}
